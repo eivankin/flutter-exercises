909f6d249505f0c69ea4ab148c4253468e96e981
Evgenij Ivankin <e.ivankin@innopolis.university>
2021-09-02 17:26:16 +0300

    third task

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,69 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        body: Container(
-          child: Column(
-            children: getWidgets(),
-            crossAxisAlignment: CrossAxisAlignment.start,
-          ),
-        ),
-      ),
+      home: MyStatefulWidget(),
     );
   }
+}
 
-  List<Widget> getWidgets() {
-    final children = <Widget>[];
-    for (int i = 0; i < 10; i++) {
-      children.add(Divider());
-      children.add(Text(i.toString()));
-    }
-    return children;
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget>
+    with TickerProviderStateMixin {
+  late final AnimationController _controller = AnimationController(
+    duration: const Duration(seconds: 2),
+    vsync: this,
+    value: 0.5
+  );
+  late final Animation<double> _animation = CurvedAnimation(
+    parent: _controller,
+    curve: Curves.easeOut,
+  );
+
+  @override
+  void initState() {
+    super.initState();
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+          child: ScaleTransition(
+            scale: _animation,
+            child: SizedBox(
+              width: 300,
+              height: 300,
+              child: const DecoratedBox(
+                decoration: const BoxDecoration(
+                    color: Colors.red
+                ),
+              ),
+            ),
+          ),
+        )
+    );
   }
 }
